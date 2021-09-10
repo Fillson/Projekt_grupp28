ea3d8705e580e2b790516af91f5bba86b513b119
filipsanderfelt <sanderfeltf@gmail.com>
2021-09-10 17:54:55 +0200

    Slutversion efter 09-10

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,3 +26,65 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
+
+*/
+
+var slideIndex = 0;
+showSlides();
+var slides, dots;
+
+
+
+// För knapparna
+function plusSlides(position) {
+    slideIndex += position;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    else if (slideIndex < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+}
+
+// OnClick för dotsen under slideshowen.
+function currentSlide(index) {
+    if (index > slides.length) { index = 1 }
+    else if (index < 1) { index = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index - 1].style.display = "block";
+    dots[index - 1].className += " active";
+}
+
+// för auto slideshow
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("hero-slides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000);
+}
+
+/* function pauseSlide() {
+    clearTimeout(timer);
+} */
