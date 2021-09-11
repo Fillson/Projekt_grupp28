5671b7903ae6846407cf22a0a71ae62be67d36c7
filipsanderfelt <Filip.sand@hotmail.com>
2021-09-11 15:40:05 +0200

    Tog bort test kod

--- script.js
+++ script.js
@@ -1,39 +1,8 @@
-/*
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("hero-slides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-
-*/
-
 var slideIndex = 0;
 showSlides();
 var slides, dots;
 
-// För knapparna
+// For the manual slideshow buttons.
 function plusSlides(position) {
   slideIndex += position;
   if (slideIndex > slides.length) {
@@ -51,7 +20,7 @@ function plusSlides(position) {
   dots[slideIndex - 1].className += " active";
 }
 
-// OnClick för dotsen under slideshowen.
+// OnClick function for the dots below the slideshow.
 function currentSlide(index) {
   if (index > slides.length) {
     index = 1;
@@ -68,7 +37,7 @@ function currentSlide(index) {
   dots[index - 1].className += " active";
 }
 
-// för auto slideshow
+// function for the automatic slideshow
 function showSlides() {
   var i;
   slides = document.getElementsByClassName("hero-slides");
@@ -87,86 +56,3 @@ function showSlides() {
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 5000);
 }
-
-showSlides();
-/*
-const slider = document.querySelector(".hero");
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-const slides = document.querySelectorAll(".hero-slides");
-const slideIcons = document.querySelectorAll(".slide-icon");
-const numberOfSlides = slides.length;
-var slidesNumber = 0;
-
-// img slider next btn
-nextBtn.addEventListener("click", function () {
-  slides.forEach((slide) => {
-    slides.classList.remove("active");
-  });
-  slideIcons.forEach((slideDot) => {
-    slideIcons.classList.remove("active");
-  });
-
-  slidesNumber++;
-
-  if (slidesNumber > numberOfSlides - 1) {
-    slidesNumber = 0;
-  }
-
-  slides[slidesNumber].classList.add("active");
-  slideDots[slidesNumber].classList.add("active");
-});
-
-// img slider prev btn
-prevBtn.addEventListener("click", () => {
-  slides.forEach((slide) => {
-    slides.classList.remove("active");
-  });
-  slideIcons.forEach((slideIcon) => {
-    slideIcons.classList.remove("active");
-  });
-
-  slidesNumber--;
-
-  if (slidesNumber < 0) {
-    slidesNumber = numberOfSlides - 1;
-  }
-
-  slides[slidesNumber].classList.add("active");
-  slideIcons[slidesNumber].classList.add("active");
-});
-
-//image slider autoplay
-var playSlider;
-
-var repeater = () => {
-  playSlider = setInterval(function () {
-    slides.forEach((slide) => {
-      slides.classList.remove("active");
-    });
-    slideIcons.forEach((slideIcon) => {
-      slideIcons.classList.remove("active");
-    });
-
-    slidesNumber++;
-
-    if (slidesNumber > numberOfSlides - 1) {
-      slidesNumber = 0;
-    }
-
-    slides[slidesNumber].classList.add("active");
-    slideIcons[slidesNumber].classList.add("active");
-  }, 4000);
-};
-repeater();
-
-// Stop the img auto play
-slider.addEventListener("mouseover", () => {
-  clearInterval(playSlider);
-});
-
-// Start the image slider autoplay again on mouseout
-slider.addEventListener("mouseout", () => {
-  repeater();
-});
-*/
